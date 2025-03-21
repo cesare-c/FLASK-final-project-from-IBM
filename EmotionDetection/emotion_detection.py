224bb6e3c05b397bea4dddcffff53da2f5a3a721
Cezar Cernescu <127303567+cesare-c@users.noreply.github.com>
2025-03-21 08:58:09 +0100

    Create emotion_detection.py
    Create an emotion detection application using the Watson NLP library

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj  = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Send a POST request to the API with the text and headers
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    status_code = response.status_code
+    emotion = {}
+    
+    if status_code == 200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        emotion = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            }
+        dominant_score = max(emotion, key=emotion.get)
+        emotion.update({'dominant_emotion': dominant_score})
+
+    elif response.status_code == 400:
+        emotion = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            }
+
+    return emotion
+
+
+    
